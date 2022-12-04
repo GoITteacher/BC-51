d3e9d8503bf9d18c25a627ce80cf70b5e2d01eb5
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2022-12-04 14:59:00 +0200

    update

--- src/scripts/01-throttle-debounce.js
+++ src/scripts/01-throttle-debounce.js
@@ -4,30 +4,3 @@ const outputElem = document.querySelector('.js-output');
 
 let countMouseEvent = 0;
 let countInputEvent = 0;
-
-// ===============================================
-const myMouseMoveEv = _.throttle(onMouseMove, 300);
-
-document.addEventListener('mousemove', myMouseMoveEv);
-
-function onMouseMove() {
-  countMouseEventElement.textContent = ++countMouseEvent;
-}
-// ===============================================
-
-// ===============================================
-
-const options = {
-  leading: true,
-  maxWait: 2000,
-  trailing: true,
-};
-
-const myInputChagneEve = _.debounce(onInputChange, 300, options);
-
-inputElement.addEventListener('input', myInputChagneEve);
-
-function onInputChange(e) {
-  outputElem.textContent = ++countInputEvent;
-}
-// ===============================================
--- src/scripts/02-filter.js
+++ src/scripts/02-filter.js
@@ -13,24 +13,3 @@ let list = [
 
 let filterElem = document.querySelector('.js-filter');
 let listElem = document.querySelector('.js-list');
-showArr(list);
-
-const debouncedOnInputChange = _.debounce(onInputChange, 300);
-
-filterElem.addEventListener('input', debouncedOnInputChange);
-
-function onInputChange(e) {
-  const filterValue = e.target.value.toLowerCase();
-  const filteredArr = list.filter(value => {
-    return value.label.toLowerCase().includes(filterValue);
-  });
-  showArr(filteredArr);
-}
-
-function showArr(arr) {
-  listElem.innerHTML = arr
-    .map(item => {
-      return `<li>${item.label}</li>`;
-    })
-    .join('');
-}
