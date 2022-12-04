8a06fb84ba0c37d73c61de8d133cda76763e7888
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2022-12-04 13:32:12 +0200

    update

--- src/js/02-calculate-total-price.js
+++ src/js/02-calculate-total-price.js
@@ -5,30 +5,8 @@
 
 const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
 
-// function calculateTotalPrice(items) {
-//   let total = 0;
-
-//   for (const item of items) {
-//     total += item;
-//   }
-
-//   return total;
-// }
-
-const calculateTotalPrice = function (items) {
-  let total = 0;
-
-  for (const item of items) {
-    total += item;
-  }
-
-  return total;
-};
-
 let total1 = calculateTotalPrice(cart);
 
 let total2 = calculateTotalPrice(cart);
 
 let total3 = calculateTotalPrice(cart);
-
-console.log(total3);
