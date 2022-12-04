a25dc9ae44292d464ba3650ac360481fec738df4
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2022-12-04 13:28:57 +0200

    update

--- src/js/01-1-creation.js
+++ src/js/01-1-creation.js
@@ -19,67 +19,6 @@
 Доступ до властивостей об'єкта в методах
 */
 
-let user = {
-  name: 'Volodka',
-  age: 23,
-  isVip: false,
-  adress: {
-    country: 'Ukraine',
-    city: 'Dnipro',
-    street: '....',
-    house: {
-      first: '1',
-      second: '2',
-    },
-  },
-  getName() {
-    console.log(this.name);
-  },
-};
-
-let user2 = {
-  name: 'Volodka2',
-  age: 23,
-  isVip: false,
-  adress: {
-    country: 'Ukraine',
-    city: 'Dnipro',
-    street: '....',
-    house: {
-      first: '1',
-      second: '2',
-    },
-  },
-  getName() {
-    console.log(this.name);
-    console.log(this.getAge());
-  },
-
-  getAge() {
-    return this.age;
-  },
-};
-
-// const playlist = {
-//   name: 'Мой супер плейлист',
-//   rating: 5,
-//   tracks: ['трек-1', 'трек-2', 'трек-3'],
-//   trackCount: 3,
-// };
-
-let user3 = {
-  ['key']: 'test key',
-};
-
-user['1,2,3,4'];
-
-const playlist = {
-  name: 'Мой супер плейлист',
-  rating: 5,
-  tracks: ['трек-1', 'трек-2', 'трек-3'],
-  trackCount: 3,
-};
-
 /*
  * Ссылочный тип {} === {}
  */
