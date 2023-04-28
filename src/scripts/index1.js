ec48b66e64918497c3942ed60a490884baedc1ac
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-04-28 13:02:06 +0300

    update

---
+++ src/scripts/index1.js
@@ -0,0 +1,7 @@
+import { user, sayHi } from './student.js';
+import * as params from './student.js';
+
+import sjkhfkhawkjdhakw from './user.js';
+
+console.log(user, sayHi);
+console.log(params);
--- src/scripts/student.js
+++ src/scripts/student.js
@@ -1,19 +1,4 @@
-export class Student {
-  name;
-  age;
-  university;
-  phoneNumber;
-
-  constructor(name, age, university, phoneNumber) {
-    this.age = age;
-    this.name = name;
-  }
-
-  sayName() {
-    console.log('Hello', this.name);
-  }
-}
-
-export function templateMarkup(arr) {
-  return arr.map(el => `<li>${el}</li>`).join('');
-}
+export const user = 10;
+export const sayHi = () => {};
+export function test() {}
+export class Student {}
