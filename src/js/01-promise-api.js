a13dcf9dee3f9ea5514a3e5c7be3e4aeef6c5364
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-05-03 13:02:36 +0300

    update

--- src/js/01-promise-api.js
+++ src/js/01-promise-api.js
@@ -1,64 +1,59 @@
 /*
  * Создание промиса
- *  - Класс Promise
+ *  - Class Promise
  *  - resolve
  *  - reject
  *  - Promise.prototype.then(onResolve, onReject)
  */
 
-// promise.then(onFulfilled, onRejected);
-
 const promise = new Promise((resolve, reject) => {
-  const rand = Math.random();
-  setTimeout(() => {
-    if (rand > 0.5) {
-      resolve(rand);
-    } else {
-      reject('Sorry!');
-    }
-  }, 10);
+  resolve(5);
 });
 
-// promise.then(onFulfilled).catch(onRejected);
+/* function onFulfilled(result) {
+  console.log('onFulfilled -> onFulfilled');
+  console.log(`✅ ${result}`);
+}
 
-// function onFulfilled(result) {
-//   console.log('onFulfilled -> onFulfilled');
-//   console.log(`✅ ${result}`);
-// }
+function onRejected(error) {
+  console.log('onRejected -> onRejected');
+  console.log(`❌ ${error}`);
+} */
 
-// function onRejected(error) {
-//   console.log('onRejected -> onRejected');
-//   console.log(`❌ ${error}`);
-// }
+promise
+  .catch(err => {
+    console.log('catch1');
+    console.log(err);
+    return 20;
+  })
+  .then(value => {
+    console.log('then1');
+    return value * 2;
+  })
+  .catch(err => {
+    console.log('catch2');
+    console.log(err);
+    return 20;
+  })
+  .then(result => {
+    console.log(result);
+    console.log('then2');
+    console.log(result / 4);
+  })
+  .catch(err => {
+    console.log('catch3');
+    console.log(err);
+  })
+  .finally(() => {
+    console.log('The end!');
+    return 200;
+  });
 
+// console.log(resultPromise);
+
+// console.log(newPromise);
 /*
  * Цепочки промисов (chaining)
  * Promise.prototype.catch(error)
  * Promise.prototype.finally()
  */
-
-promise
-  .then(value1 => {
-    console.log(value1, '-> promise1');
-    return [value1];
-  })
-  .catch(err => {
-    console.log('Error', err);
-    return Promise.reject('err');
-  })
-  .then(value2 => {
-    if (value2.length !== 0) {
-      console.log();
-    }
-    return 'food';
-  })
-  .then(value3 => {
-    return value3;
-  })
-  .then(value4 => {
-    console.log('value4->', value4);
-  })
-  .catch(() => {
-    console.log('last catch');
-  })
-  .finally(() => {});
--- src/js/02-promisification.js
+++ src/js/02-promisification.js
@@ -4,70 +4,77 @@
  * - Функция которая возвращает промис
  */
 
+// const promise = new Promise((resolve, reject) => {
+//   const numb = Math.random();
+//   resolve(numb);
+// });
+
+/* function createPromise(value, delay, isActive) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isActive) {
+        resolve(value);
+      } else {
+        reject(value);
+      }
+    }, delay);
+  });
+  return promise;
+} */
+/* 
+const promise1 = createPromise('Hello', 1000, true);
+const promise2 = createPromise('Error', 1000, false);
+const promise3 = createPromise('Error 2', 1000, false);
+ */
 /*
  * Промисификация «синхронных» функций
  * - Promise.resolve()
  * - Promise.reject()
  */
 
-/*
- * Покемоны с https://pokeapi.co/
- */
+/* function createPromise(value) {
+  return Promise.resolve(value);
+  //   return Promise.reject('Error')
+} */
 
-// function getData(id, renderArray) {
+// const result1 = createPromise(20);
+// const result2 = createPromise();
 
-// }
+// Promise.resolve(20).then(value => console.log(value))
+// Promise.reject('Sorry')
+//   .then(value => console.log(value))
+//   .catch(console.log);
 
-// let posts;
+// =========================
 
-// getData(3).then(data => {
-//   console.log(data);
-//   console.log(data);
-//   console.log(data);
-//   renderArra();
-// });
-
-// getData(2).then(data => {
-//   posts = data;
-// });
-
-// console.log(Promise.resolve(10));
-
-// function getData(err) {
-//   return Promise.reject(err);
-// }
-
-// getData()
-//   .then(value => {
-//     console.log(value);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-// const newPromise = new Promise((res, rej) => {
-//   return res(10);
-//   console.log('end');
-// });
-
-// ===========================
-
-function createPromise(value, delay, isErr = false) {
-  return new Promise((resolve, reject) => {
+function createPromise(value, delay, isActive) {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (isErr) reject(value);
-      else resolve(value);
+      if (isActive) {
+        resolve(value);
+      } else {
+        reject(value);
+      }
     }, delay);
   });
+  return promise;
 }
 
-const p1 = createPromise('test1', 5000);
-const p2 = createPromise('test2', 2000, true);
-const p3 = createPromise('test3', 3000);
-const p4 = createPromise('test4', 4000);
+const p1 = createPromise('1', 1000, true);
+const p2 = createPromise('2', 5000, true);
+const p3 = createPromise('3', 3000, false);
+const p4 = createPromise('4', 500, false);
 
-Promise.race([p3, p1, p2, p4])
-  .then()
-  .catch(err => {
-    console.log(err);
-  });
+const promises = [p1, p2, p3, p4];
+
+console.log(promises);
+
+// Promise.all(promises).then(result => {
+//   console.log(result);
+// });
+
+// Promise.race(promises).then(res => console.log(res));
+
+Promise.allSettled(promises).then(arr => {
+  console.log(arr);
+});
