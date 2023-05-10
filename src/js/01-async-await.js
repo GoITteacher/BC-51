ceb482178899ea9d5bdc49481a7bb8e9b19fc758
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-05-10 11:24:03 +0300

    update

--- src/js/01-async-await.js
+++ src/js/01-async-await.js
@@ -1,92 +1,46 @@
 import '../css/common.css';
 import axios from 'axios';
 
-/* fetch('users')
-  .then(response => {
-    return response.json();
-  })
-  .then(users => {
-    return fetch('movie');
-  })
-  .then(response => {
-    return response.json();
-  })
-  .then(movie => {
-    console.log(movie);
-  });
+const axiosV2 = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+});
 
-async function foo() {
-  const users = await fetch('http://localhost:4040/users').then(res =>
-    res.json(),
-  );
-  const params = users
-    .map(user => {
-      return user.books.map(id => {
-        return `id=${id}`;
-      });
-    })
-    .flat()
-    .join('&');
-  const books = await fetch(`http://localhost:4040/books?${params}`).then(res =>
-    res.json(),
-  );
-}
-
-foo();
-
-async function foo1() {
-  const users = await fetch('users').then(res => res.json());
-  const books = fetch(`/books?${params}`).then(res => res.json());
-} */
-
-// async function foo2() {
-//     fetch('users')
-//     .then(response => {
-//       return response.json();
+// function getCurrentComment(user, post, comment) {
+//   return axiosV2
+//     .get('/users')
+//     .then(({ data }) => {
+//       const userObj = data[user - 1];
+//       return axiosV2.get(`/posts?userId=${userObj.id}`);
 //     })
-
-//     await fetch('users');
-//     console.log('Ok');
-
-//     fetch('users')
-//     .then(()=>{
-//       console.log('Ok');
+//     .then(({ data: data2 }) => {
+//       const postElem = data2[post - 1];
+//       return axiosV2.get(`/comments?postId=${postElem.id}`);
 //     })
-
+//     .then(({ data: data3 }) => {
+//       const commentElem = data3[comment - 1];
+//       return commentElem;
+//     });
 // }
 
-// TRY CATCH
-
-/* async function testFoo() {
+async function getCurrentComment(user, post, comment) {
   try {
-    const res1 = await fetch(`http://loalhost:4040/boo`);
-    console.log('end');
-  } catch {}
+    const { data } = await axiosV2.get('/users');
+    const userObj = data[user - 1];
+
+    const { data: data2 } = await axiosV2.get(`/posts?userId=${userObj.id}`);
+    const postElem = data2[post - 1];
+
+    const { data: data3 } = await axiosV2.get(
+      `/comments?postId=${postElem.id}`,
+    );
+
+    const commentElem = data3[comment - 1];
+    return commentElem;
+  } catch {
+    return null;
+  }
 }
 
-try {
-  setTimeout(() => {
-    console.log('err');
-  }, 5000);
-} catch {
-  console.log('test Fooo Err');
-}
-
-console.log('end');
- */
-
-async function foo() {
-  const prom1 = fetch(`http://localhost:4040/books`);
-  const prom2 = fetch(`http://localhost:4040/users`);
-  const prom3 = fetch(`http://localhost:4040/rooms`);
-
-  const users = await fetch('users');
-  const promises = [prom1, prom2, prom3];
-  const response = await Promise.all(promises);
-
-  console.log(response);
-  // response = await Promise.all(promises);
-  //console.log(response);
-}
-
-foo();
+getCurrentComment(20, 2, 4).then(comment => {
+  console.log(comment);
+});
